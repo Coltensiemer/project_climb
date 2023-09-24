a9a175815300ae3ca2b4d2361550714cf62ba05c
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-09-24 11:26:31 -0400

    updated modules in package.json

--- src/utils/clusters.js
+++ src/utils/clusters.js
@@ -1,62 +1,53 @@
-const { Cluster } = require('puppeteer-cluster');
-const scrapeEvents = require('./puppeteer');
-const { data } = require('autoprefixer');
+// const { Cluster } = require('puppeteer-cluster');
+import { Cluster } from "puppeteer-cluster";
+import  scrapeEvents  from "./puppeteer.js";
 
 async function clusters() {
+  const eventListAll = await scrapeEvents();
+  const urls = [];
 
-const eventListAll = await scrapeEvents()
-const urls = []
-
-await eventListAll.map((event) => {
-	const {title, urlResults} = event
-	urls.push(urlResults)
-})
-
-
+  await eventListAll.map((event) => {
+    const { title, urlResults } = event;
+    urls.push(urlResults);
+  });
 
   const cluster = await Cluster.launch({
     concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency:5,
-	monitor: true,
-	puppeteerOptions: { 
-		headless: 'new'
-	}
+    maxConcurrency: 5,
+    monitor: true,
+    puppeteerOptions: {
+      headless: "new",
+    },
   });
 
-  cluster.on('taskerror', (err, data) => {
-	console.log(`Error crawling ${data}: ${err.message}`);
-});
-
+  cluster.on("taskerror", (err, data) => {
+    console.log(`Error crawling ${data}: ${err.message}`);
+  });
+// Run each URL and gather info
   await cluster.task(async ({ page, data: url }) => {
     await page.goto(url);
-	await page.waitForNetworkIdle()
-	const data = await page.evaluate(() => {
-		const compName = document.getElementById('compName');
-		const compLocation = document.getElementById('compLocation')
-		const compDate = document.getElementById('compDates')
+    await page.waitForNetworkIdle();
+    const data = await page.evaluate(() => {
+      const compName = document.getElementById("compName");
+      const compLocation = document.getElementById("compLocation");
+      const compDate = document.getElementById("compDates");
 
-		return data = { 
-			compName: compName.innerText,
-			compLocation: compLocation.innerText,
-			compDate: compDate.innerText
-		}
-		
-	  });
-	
-	  console.log('results', data);
-	
-	});
+      return (data = {
+        compName: compName.innerText,
+        compLocation: compLocation.innerText,
+        compDate: compDate.innerText,
+      });
+    });
 
+    console.log("results", data);
+  });
 
-
-  for(const url of urls) { 
-	await cluster.queue(url)
-
-}
-
+  for (const url of urls) {
+    await cluster.queue(url);
+  }
 
   await cluster.idle();
   await cluster.close();
 }
 
-clusters()
+clusters();
--- src/utils/puppeteer.js
+++ src/utils/puppeteer.js
@@ -1,59 +1,26 @@
+// const puppeteer= require("puppeteer");
+import puppeteer from "puppeteer";
 
-
-const puppeteer= require("puppeteer");
-const cluster = require("puppeteer-cluster"); 
-
-
-
-
-
-//WebScrapping for EventList, to be export into clusters.js to run mutiple clusters 
-async function scrapeEvents() {
+//WebScrapping for EventList, to be export into clusters.js to run mutiple clusters
+export default async function scrapeEvents() {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
   // Navigate the page to a URL
   await page.goto("https://usacresults.org");
-  await page.waitForNetworkIdle()
+  await page.waitForNetworkIdle();
 
-  await page.screenshot({path: 'practice.png', fullPage: true})
+  await page.screenshot({ path: "practice.png", fullPage: true });
 
-  const eventListAll = await page.evaluate(() => Array.from(document.querySelectorAll('.eventList li'), (e) => ({
-	title: e.querySelector('a')?.innerText,
-	urlResults: e.querySelector('a')?.href 
-  })))
-
+  const eventListAll = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".eventList li"), (e) => ({
+      title: e.querySelector("a")?.innerText,
+      urlResults: e.querySelector("a")?.href,
+    })),
+  );
 
   await browser.close();
 
-  return eventListAll
+  return eventListAll;
 }
-
-module.exports = scrapeEvents; 
-
-
-
-
-
-
-
-
-
-
-// const scrapePage = async(url) => {
-//   const browser = await puppeteer.launch({ headless: "new" });
-//   const page = await browser.newPage();
-//   await page.goto(url);
-//   await page.waitForSelector('#compLocation');
-//   const pageTitle = await page.title();
-//   console.log(`Page Title: ${pageTitle}`);
-//   const data = await page.evaluate(() => Array.from(document.getElementById('compLocation'), (e) => ({text: 'hello'}))) 
-//   await browser.close();
-//   return data
-// }
-// await Promise.all(eventListAll.slice(0,5).map(( async({results}) => { 
-// console.log('Results', results)
-// const data = await scrapePage(results)
-// console.log('data', data)
-// }))) 
