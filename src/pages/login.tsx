1bcdd5f11b5a2704c0d2a9188d9666aefd694e32
Colten Siemer <110078481+Coltensiemer@users.noreply.github.com>
2023-10-06 15:10:14 -0400

    added login component

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,10 +5,8 @@ import { Button } from "@/components/ui/button";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({
-    username: "",
     email: "",
     password: "",
-    confirmPassword: "",
   });
 
   const handleChange = (e: string) => {};
@@ -20,24 +18,8 @@ export default function SignUp() {
       <Header />
       <div className="mt-10 flex justify-center">
         <div className="w-full max-w-md">
-          <h1 className="mb-6 text-2xl font-semibold">Sign Up</h1>
+          <h1 className="mb-6 text-2xl font-semibold">Login</h1>
           <form onSubmit={undefined}>
-            <div className="mb-4">
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Username
-              </label>
-              <input
-                type="text"
-                id="username"
-                name="username"
-                value={formData.username}
-                onChange={undefined}
-                className="mt-1 w-full rounded-md border p-2 focus:border-blue-500 focus:outline-none"
-              />
-            </div>
             <div className="mb-4">
               <label
                 htmlFor="email"
@@ -70,32 +52,17 @@ export default function SignUp() {
                 className="mt-1 w-full rounded-md border p-2 focus:border-blue-500 focus:outline-none"
               />
             </div>
-            <div className="mb-4">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-               Confirm Password
-              </label>
-              <input
-                type="confirmPassword"
-                id="confirmPassword"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={undefined}
-                className="mt-1 w-full rounded-md border p-2 focus:border-blue-500 focus:outline-none"
-              />
-            </div>
+           
             <Button
               type="submit"
               className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none"
             >
-              Sign Up
+              Login
             </Button>
             <div className="mt-5">
-              <p>Already have an account?</p>
+              <p>Don't Have an account?</p>
               <Button variant={"outline"}>
-                <Link href='./login'>Login</Link>
+                <Link href='signup'>Sign Up</Link>
               </Button>
             </div>
           </form>
